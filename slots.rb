f99c31ec767bbec85eb95a40564c1c96fa7194e6
Erik McComb <erik@erikmccomb.com>
2017-08-21 18:30:17 -0600

    working slots

--- slots.rb
+++ slots.rb
@@ -1,103 +1,138 @@
+require 'pry'
+
 #slot machine roll
+
 class Slots
+  attr_accessor :player
+  
+  @slot_cash = []
 
-  @chances = [ 'cherry', 'cherry', 'bar', 'bar', 'bar', 'bar', 'bar',
-    7, 7, 7, 7, 7, 7, 7, 7, 'cherry', 'cherry', 
-    'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 
-    'cherry', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar',
-    'bar', 'bar', 'bar', 'bar', 'bar', 'bar',
-    'trplbar', 'trplbar', 'trplbar', 'trplbar', 'trplbar', 'trplbar',
-    'trplbar', 'trplbar', 'trplbar', 'trplbar', 'trplbar', 
-    'jackpot', 'jackpot', 
-  ]
-
-  def slot_menu
+  
+  def initialize(player)
+    puts "Welcome to HOT SHOT Slots, #{player.name}!"
+    puts 'Youll have to spin it to win it!'
+    puts "You have #{player.wallet.amount} to bet with!"
+    slot_menu(player)
+  end
+  
+  def slot_menu(player)
+    @slot_cash = player.wallet.amount
     puts 'place your bets, or walk away!'
+    puts "Reminder: You have $#{player.wallet.amount} left."
     puts '1) Place bet'
     puts '2) Leave Slots'
     case gets.to_i
       when 1
-        machine
+        machine(player)
       when 2
+        exit
       else
         puts 'Invalid choice! Try again!'
-        slot_menu
+        slot_menu(player)       
     end
   end
 
-  def machine
+  def machine(player)
     puts 'How much you betting?'
-    puts "Reminder: You have #{@slot_cash} left."
-    puts '1) $5'
-    puts '2) $10'
-    puts '3) $20'
+
+    puts '1) $1'
+    puts '2) $5'
+    puts '3) $10'
+    puts '4) $20'
+    puts '5) Nevermind, main menu'
     user_input = gets.to_i
     case user_input
       when 1
-        if @slot_cash < 5
+        if player.wallet.amount < 1
           puts 'Sorry! you dont have enough left to make that bet!'
-          machine
+          machine(player)
         end
-        @bet = 5
+          @bet = 1
       when 2
-        if @slot_cash < 10
+        if player.wallet.amount < 5
           puts 'Sorry! you dont have enough left to make that bet!'
-          machine
+          machine(player)
         end
-        @bet = 10
+          @bet = 5
       when 3
-        if @slot_cash < 20
+        if player.wallet.amount < 10
           puts 'Sorry! you dont have enough left to make that bet!'
-          machine
+          machine(player)
         end
-        @bet = 20
+          @bet = 10
+      when 4
+        if player.wallet.amount < 20
+          puts 'Sorry! you dont have enough left to make that bet!'
+          machine(player)
+        end
+          @bet = 20
+      when 5
+        slot_menu(player)
       else
         puts 'Invalid bet! Try again!'
-        machine
+        machine(player)
     end
     puts "Bets are in; Pull that lever!"
+    roll(player)
   end
 
-  def initialize(player)
-    puts 'Welcome to HOT SHOT Slots!'
-    puts 'Youll have to spin it to win it!'
-    puts "Welcome to High Low #{player.name}"
-    puts "You have #{player.wallet.amount} to bet with!"
-    @slot_cash = player.wallet.amount
+  
+  def roll(player)
+    @chances = [ 'cherry', 'cherry', 'bar', 'bar', 'bar', 'bar', 'bar', 7, 7, 7, 7, 7, 7, 7, 7, 'dblbar', 'dblbar', 
+    'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 'dblbar', 
+    'bar', 'bar', 'bar', 'bar', 'bar', 'bar', 'trplbar', 'trplbar', 'trplbar', 'trplbar', 'trplbar', 'trplbar', 
+    'trplbar', 'trplbar', 'trplbar', 'trplbar', 'trplbar', 'jackpot', 'jackpot' ] 
+
+    @roller_results = []
+    @roller_results << @chances.sample
+    @roller_results << @chances.sample
+    @roller_results << @chances.sample
+    case @roller_results
+      when [ 'cherry', 'cherry', 'cherry' ]
+        @bet_result = @bet
+        puts 'You broke even!'
+        player.wallet.amount = @slot_cash.to_i + @bet_result.to_i
+        win(player)
+      when [ 'bar', 'bar', 'bar' ]
+        @bet_result = @bet * 2
+        puts 'Doubled your dough! Well done!'
+        player.wallet.amount = @slot_cash.to_i + @bet_result.to_i
+        win(player)
+      when [ 'dblbar', 'dblbar', 'dblbar']
+        @bet_result = @bet * 5
+        puts 'Your bet back times 5! Feelin alive?!'
+        player.wallet.amount = @slot_cash.to_i + @bet_result.to_i
+        win(player)
+      when [ 'trplbar', 'trplbar', 'trplbar' ]
+        @bet_result = @bet * 20
+        puts 'You are on a roll! Bet some more!'
+        player.wallet.amount = @slot_cash.to_i  + @bet_result.to_i 
+        win(player)
+      when [ 7, 7, 7 ]
+        @bet_result = @bet * 65
+        puts 'Big winner! Now keep at it for that jackpot!'
+        player.wallet.amount = @slot_cash.to_i + @bet_result.to_i
+        win(player)
+      when [ 'jackpot', 'jackpot', 'jackpot' ]
+        @bet_result = @bet * 100
+        puts 'JACKPOT!'
+        player.wallet.amount = @slot_cash.to_i + @bet_result.to_i
+        win(player)
+      else
+        puts "#{@roller_results[0]} - #{@roller_results[1]} - #{@roller_results[2]}"
+        puts 'Better luck next time dude!'
+        player.wallet.amount = @slot_cash.to_i - @bet
+        puts "You now have $#{player.wallet.amount} in your wallet"
+        slot_menu(player)
+      end
+    machine(player)
+  end
+
+  def win(player)
     binding.pry
-    slot_menu
-  end
-
-  def machine
-    puts 'How much you betting?'
-    puts "Reminder: You have #{@slot_cash} left."
-    puts '1) $5'
-    puts '2) $10'
-    puts '3) $20'
-    user_input = gets.to_i
-    case user_input
-      when 1
-        if @slot_cash < 5
-          puts 'Sorry! you dont have enough left to make that bet!'
-          machine
-        end
-        @bet = 5
-      when 2
-        if @slot_cash < 10
-          puts 'Sorry! you dont have enough left to make that bet!'
-          machine
-        end
-        @bet = 10
-      when 3
-        if @slot_cash < 20
-          puts 'Sorry! you dont have enough left to make that bet!'
-          machine
-        end
-        @bet = 20
-      else
-        puts 'Invalid bet! Try again!'
-        machine
-    end
-    puts "Bets are in; Pull that lever!"
+    puts "#{@roller_results[0]} - #{@roller_results[1]} - #{@roller_results[2]}"
+    puts "You bet #{@bet}, and you won #{@bet_result}!!!"
+    puts "You now have $#{player.wallet.amount} in your wallet"
+    slot_menu(player)
   end
 end
